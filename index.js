3f11a6fdc93c2940e65644ae0d1734e41f6224c3
Latya-fay <ndanumulatya7@gmail.com>
2024-12-16 23:09:16 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
-  //add static methods here
+  // Static method to capitalize the first letter of a string
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  // Static method to sanitize a string
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-' ]/g, '');
+  }
+
+  // Static method to titleize a sentence
+  static titleize(sentence) {
+    const doNotCapitalize = [
+      'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'
+    ];
+
+    const words = sentence.split(' ');
+
+    return words
+      .map((word, index) => {
+        if (index === 0 || !doNotCapitalize.includes(word.toLowerCase())) {
+          return this.capitalize(word); 
+        } else {
+          return word;
+        }
+      })
+      .join(' ');
+  }
 }
